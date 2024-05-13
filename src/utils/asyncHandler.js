2be9904140115374d0ddf4cef72272e2752490e6
Vineeshe <vineeshryl@gmail.com>
2024-05-13 16:47:52 +0530

    All setup completed

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,23 +1,11 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).
-            catch((err) => next(err))
-    }
-}
-
-
-
-
-
-
-// const asyncHandler = (requestHandler) => {
-//     return async (req, res, next) => {
-//         try {
-//             await Promise.resolve(requestHandler(req, res, next));
-//         } catch (err) {
-//             next(err);
-//         }
-//     };
-// };
+    return async (req, res, next) => {
+        try {
+            await Promise.resolve(requestHandler(req, res, next));
+        } catch (err) {
+            next(err);
+        }
+    };
+};
 
 export { asyncHandler };
